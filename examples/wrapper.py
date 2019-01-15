cb76e67327600962bd943454b0d9f154eb2e3f6e
Michael McCune <msm@redhat.com>
2019-01-15 14:37:47 -0500

    update example udf
    Fixes some bugs and adds error protection to the function, this is a
    better example.

--- examples/wrapper.py
+++ examples/wrapper.py
@@ -1,7 +1,14 @@
 import json
 
 def main(value):
-    current = json.loads(value)
-    wrapped = {'wrapped': current}
+    data = value
+    if data == '':
+        ret = {'empty': ''}
+    else:
+        try:
+            current = json.loads(data)
+        except Exception:
+            current = data
+        wrapped = {'wrapped': current}
     ret = json.dumps(wrapped)
     return ret
