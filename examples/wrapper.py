b34b92b1d3895e26f555f4dbac4ce9425e556f2d
Michael McCune <msm@redhat.com>
2019-01-17 13:58:57 -0500

    add docstring to example udf

--- examples/wrapper.py
+++ examples/wrapper.py
@@ -1,6 +1,16 @@
 import json
 
 def user_defined_function(value):
+    """Return a string-based JSON object
+
+    This function accepts a string value and will return a string containing
+    a JSON object. The returned object simply take the input string and
+    puts it into a JSON object with the key `wrapped`.
+
+    User defined functions must accept a single value (a string) and return
+    either a string value or None. If None is returned there will be no
+    message passed along the stream topic.
+    """
     data = value
     if data == '':
         ret = {'empty': ''}
